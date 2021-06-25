54645be392a9724943a3ba9acfc4aa1bebba3a53
Arash Aria <aarash32322@gmail.com>
2021-06-25 20:59:54 +0300

    adding 'expectationFailOutput' message

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -7,14 +7,14 @@ describe('Calcultor Service', () => {
     const calculator = new CalculatorService(new LoggerService());
 
     const result = calculator.add(2, 2);
-    expect(result).toBe(4);
+    expect(result).toBe(4, 'unexpected addition results');
   });
 
   it('Should subtract two numbers', () => {
     const calculator = new CalculatorService(new LoggerService());
 
     const result = calculator.subtract(6, 4);
-    expect(result).toBe(2);
+    expect(result).toBe(2, 'unexpected subtraction results');
   });
 
 });
