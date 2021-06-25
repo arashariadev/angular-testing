5aa0892a70a1bc3f02d804264352ba68321bc618
Arash Aria <aarash32322@gmail.com>
2021-06-25 20:52:27 +0300

    first test simple case

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,20 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
+describe('Calcultor Service', () => {
+
+  it('Should add two numbers', () => {
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.add(2, 2);
+    expect(result).toBe(4);
+  });
+
+  it('Should subtract two numbers', () => {
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.subtract(6, 4);
+    expect(result).toBe(2);
+  });
+
+});
