0fd3cfb46df6680cff304d38a4162b2af979938a
Arash Aria <aarash32322@gmail.com>
2021-06-27 14:54:40 +0300

    Beginning of async test - fails when attempted synchronously

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -92,7 +92,18 @@ describe('HomeComponent', () => {
 
   it("should display advanced courses when tab clicked", () => {
 
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+    // el.nativeElement.click();
+    click(tabs[1]); // simulating click on the second button (advanced tab)
+    fixture.detectChanges();
+
+    const cardTitles = el.queryAll(By.css('.mat-card-title'));
+
+    expect(cardTitles.length).toBeGreaterThan(0, 'Could not find titles in advanced tab');
+    expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course', 'Course Title is different!');
 
   });
 
