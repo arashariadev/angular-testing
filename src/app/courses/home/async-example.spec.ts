526064f57f45c506313afe0c54cdfb4fcbe19290
Arash Aria <aarash32322@gmail.com>
2021-06-27 21:12:41 +0300

    example test case for micro & macro tasks

--- src/app/courses/home/async-example.spec.ts
+++ src/app/courses/home/async-example.spec.ts
@@ -1,6 +1,6 @@
-import { fakeAsync, tick, flush } from "@angular/core/testing";
+import { fakeAsync, tick, flush, flushMicrotasks } from "@angular/core/testing";
 
-describe('Async testing examples', () => {
+fdescribe('Async testing examples', () => {
 
   it('Async test example with done function', (done: DoneFn) => {
     let test: boolean = false;
@@ -15,6 +15,8 @@ describe('Async testing examples', () => {
   // using fakeAsync and tick
   it('Async test example - fakeAsync and tick', fakeAsync(() => {
     let test: boolean = false;
+
+    setTimeout(() => {});
 
     setTimeout(() => {
       test = true;
@@ -35,6 +37,41 @@ describe('Async testing examples', () => {
 
   flush();
   expect(test).toBeTruthy();
+  }));
+
+  // promise based test case
+  it('Async test example - plain promise based', fakeAsync(() => {
+    let test: boolean = false;
+
+    Promise.resolve().then(() => {
+      return Promise.resolve();
+    }).then(() => {
+      test = true;
+    });
+
+    flushMicrotasks(); //promise is micro-task where as setTimeout, setInterval, click , etc are macro-task
+    expect(test).toBeTruthy();
+  }));
+
+  it('Async test example - with promise and setTimeout', fakeAsync(() => {
+    let counter: number = 0;
+
+    Promise.resolve().then(() => {
+      counter += 10;
+
+      setTimeout(() => {
+        counter += 1;
+      }, 1000);
+    });
+
+    expect(counter).toBe(0);
+    flushMicrotasks();
+    expect(counter).toBe(10);
+    tick(500);
+    expect(counter).toBe(10);
+    tick(500);
+    expect(counter).toBe(11);
+
   }));
 
 
